bec955256a04ef136cb8c82d2c383d80518a09d5
jvmqueue <wheels0090@yahoo.com>
2015-06-19 15:07:35 -0400

    define listenerPopulateMainContent for home.html template

--- scripts/template.js
+++ scripts/template.js
@@ -1,30 +1,72 @@
 define(['underscore' ,'util', 'xml', 'regex'], function(_, util, xml, regEx){
+    var w = window, d = document;
     var listenerPopulateButtons = function(e, paramTemplate){
-        var w = window, d = document;
         var data = e.data;
         var templateHtml = paramTemplate;
         var strIdTemplate = regEx.fnc.getIdFromString(templateHtml);
         var strTemplateHtml = $(templateHtml).html(); // our custom template, not the entire response text
         var strFragment = '';
         var strButtonValue = '';
+        var strId = '';
         var template = _.template(strTemplateHtml);
         var $nodeExist = $('#buttons');
         
         for(var i = 0, len = data.length; i < len; i++){
                strButtonValue = data[i].button; // data from JSON so button[i] is next element. Elements are hashes having name value pairs
-               strFragment += template( btnValue = strButtonValue ); // btnValue is variable defined in template
+               strId = 'btn' + i;               
+               strFragment += template(btnId = strId, btnValue = strButtonValue); // btnValue is variable defined in template
+        }
+
+        $nodeExist.html(strFragment);
+    };
+    var listenerPopulateMainContent = function(e, paramTemplate){
+        var data = e.data;
+        var templateHtml = paramTemplate;
+        var strIdTemplate = regEx.fnc.getIdFromString(templateHtml);
+        var strTemplateHtml = $(templateHtml).html(); // our custom template, not the entire response text
+        var strFragment = '';
+        var strLineItemValue = '';
+        var strId = '';
+        var template = _.template(strTemplateHtml);
+        var $nodeExist = $('#mainContent'); // TODO: should be defined via parameter
+
+        for(var i = 0, len = data.length; i < len; i++){
+               strLineItemValue = data[i].lineItem; // data from JSON so button[i] is next element. Elements are hashes having name value pairs           
+               strFragment += template(lineItemValue = strLineItemValue); // btnValue is variable defined in template
         }
 
         $nodeExist.html(strFragment);
     };
     var _fnc = {
+        populateMainContent:function(options){
+            var $nodeExist = options.$nodeContainer;
+            var hash = options.hashNameValues;
+            var strEvent = 'retrieved:template';
+            var strSelector = '#' + $nodeExist.attr('id');
+
+            util.fnc.setListener({
+                selector:strSelector,
+                event:strEvent,
+                data:hash,
+                listener:listenerPopulateMainContent
+            });
+            var req = util.fnc.getData({
+                path:'templates/home.html',
+                cache:false,
+                fileType:'text',
+                $node:$nodeExist,
+                event:strEvent
+            });                                
+
+
+        },
         populateControls:function(options){
             var $nodeExist = options.$nodeContainer;
             var hash = options.hashNameValues;
             var strEvent = 'retrieved:template';
             var strSelector = '#' + $nodeExist.attr('id');
-            // TODO: get template via util
-
+            
+            // get template via util
             util.fnc.setListener({
                 selector:strSelector,
                 event:strEvent,
