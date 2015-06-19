1012ce46588b609f7968eecf661119a82d80ef57
jvmqueue <wheels0090@yahoo.com>
2015-06-19 11:30:27 -0400

    code clean up. Add w and d vars in case we need access to window of document object

--- scripts/template.js
+++ scripts/template.js
@@ -1,20 +1,20 @@
 define(['underscore' ,'util', 'xml', 'regex'], function(_, util, xml, regEx){
     var listenerPopulateButtons = function(e, paramTemplate){
+        var w = window, d = document;
         var data = e.data;
         var templateHtml = paramTemplate;
-        
-
         var strIdTemplate = regEx.fnc.getIdFromString(templateHtml);
         var strTemplateHtml = $(templateHtml).html(); // our custom template, not the entire response text
         var strFragment = '';
         var strButtonValue = '';
         var template = _.template(strTemplateHtml);
+        var $nodeExist = $('#buttons');
+        
         for(var i = 0, len = data.length; i < len; i++){
                strButtonValue = data[i].button; // data from JSON so button[i] is next element. Elements are hashes having name value pairs
                strFragment += template( btnValue = strButtonValue ); // btnValue is variable defined in template
         }
 
-        var $nodeExist = $('#buttons');
         $nodeExist.html(strFragment);
     };
     var _fnc = {
